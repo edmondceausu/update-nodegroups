1bedb202d281a81dc64dd678b628ddf5e4d02a76
Edmond Ceausu <eceausu@amazon.com>
2024-03-02 18:23:00 -0500

    Create update-nodegroups.sh

---
+++ update-nodegroups.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get a list of all EKS clusters in the region
+REGION=us-east-2
+clusters=($(aws eks list-clusters --region ${REGION} --output json | jq -r '.clusters[]'))
+
+# Loop through each cluster
+for cluster in ${clusters[@]}; do
+    # Get a list of all nodegroups in the cluster
+    nodegroups=($(aws eks list-nodegroups --cluster-name $cluster --region ${REGION} --output json | jq -r '.nodegroups[]'))
+
+    # skip if there are no node groups
+    echo "checking for nodegroups in $cluster"
+    if [[ ${#nodegroups[@]} -eq 0]]; then
+        echo "No nodegroups found in cluster $cluster."
+        continue
+    fi
+
+    # Loop through each nodegroup
+    for nodegroup in ${nodegroups[@]}; do
+        # Update the nodegroup
+        echo "Updating nodegroup $nodegroup in cluster $cluster..."
+        echo $nodegroup
+        aws eks update-nodegroup-version --cluster-name ${cluster} --nodegroup-name ${nodegroup} --region $REGION || echo $?
+    done
+done
